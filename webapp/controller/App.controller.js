15ce8ba18d7e1f4346d42f9f05c3cccc30d9023f
se80ps <se80@gmx.net>
2021-01-02 16:09:47 +0100

    Step 19: Reuse Dialogs

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], function (Controller ) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-       
-    });
- });
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+
+		onOpenDialog : function () {
+			this.getOwnerComponent().openHelloDialog();
+		}
+	});
+
+});
--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,46 +1,19 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast",
-	"sap/ui/core/Fragment"
-], function (Controller, MessageToast, Fragment) {
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
 	"use strict";
-
 	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
-
 		onShowHello : function () {
 			// read msg from i18n model
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
 			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
 			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
 			// show message
 			MessageToast.show(sMsg);
 		},
-
 		onOpenDialog : function () {
-			var oView = this.getView();
-
-			if (!this.pDialog) {
-				this.pDialog = Fragment.load({
-					id: oView.getId(),
-					name: "sap.ui.demo.walkthrough.view.HelloDialog",
-					controller: this
-				}).then(function (oDialog) {
-					// connect dialog to the root view of this component (models, lifecycle)
-					oView.addDependent(oDialog);
-					return oDialog;
-				});
-			} 
-			this.pDialog.then(function(oDialog) {
-				oDialog.open();
-			});
-		},
-
-		onCloseDialog : function () {
-			// note: We don't need to chain to the pDialog promise, since this event-handler
-			// is only called from within the loaded dialog itself.
-			this.byId("helloDialog").close();
+			this.getOwnerComponent().openHelloDialog();
 		}
 	});
-
 });
